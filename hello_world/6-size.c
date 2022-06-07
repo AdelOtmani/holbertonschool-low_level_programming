7d866f877dad457c9a4db6da6a2115a6171bf030
AdelOtmani <5023@holbertonschool.com>
2022-06-07 11:50:59 +0200

    script fix size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-	    int inttype;
-		float floattype;
-		long int longtype;
-		long long int llt;
-		char chartype;
+	    int a;
+		float b;
+		long int c;
+		long long int d;
+		char e;
 
-		printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(chartype));
-		printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(inttype));
-		printf("Size of a long int: %lu byte(s)\n", (unsigned int)sizeof(longtype));
-		printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(llt));
-		printf("Size of a float: %lu byte(s)\n", (unsigned int)sizeof(floattype));
+		printf("Size of a char: %lu byte(s)\n", (unsigned int)sizeof(e));
+		printf("Size of an int: %lu byte(s)\n", (unsigned int)sizeof(a));
+		printf("Size of a long int: %lu byte(s)\n", (unsigned int)sizeof(c));
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned int)sizeof(d));
+		printf("Size of a float: %lu byte(s)\n", (unsigned int)sizeof(b));
 		return (0);
 }
