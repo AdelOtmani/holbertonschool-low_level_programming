b7c890146048f87e1284ad0ae7f4d5612d2edb9e
AdelOtmani <5023@holbertonschool.com>
2022-06-07 10:52:43 +0200

    script size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 		long long int longlongtype;
 		char chartype;
 
-		printf("Size of int: %lu bytes\n", sizeof(inttype));
-		printf("Size of float: %lu bytes\n", sizeof(floattype));
-		printf("Size of long int: %lu bytes\n", sizeof(longtype));
-		printf("Size of char: %lu byte\n", sizeof(charType));
-		printf("Size of a long long int: %lu bytes\n", sizeof(longlongtype));
+		printf("Size of int: %lu bytes\n", (unsigned)sizeof(inttype));
+		printf("Size of float: %lu bytes\n", (unsigned)sizeof(floattype));
+		printf("Size of long int: %lu bytes\n", (unsigned)sizeof(longtype));
+		printf("Size of char: %lu byte\n", (unsigned)sizeof(charType));
+		printf("Size of a long long int: %lu bytes\n", (unsigned)sizeof(longlongtype));
 		return (0);
 }
