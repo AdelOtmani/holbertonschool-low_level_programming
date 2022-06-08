0a36d5e16f6b25f11088cbc12aa939c1ef8b7be9
AdelOtmani <5023@holbertonschool.com>
2022-06-08 15:09:25 +0200

    script putchar4

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	int a;
 
-	for (a = '0'; a >= '9'; a++)
+	for (a = '0'; a >= '9'; a++){
 		putchar(a);
-	putchar(',');
-	putchar(' ');
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
