dd6319c0da8c0b8023230c5ff7fa624badaba8ca
AdelOtmani <5023@holbertonschool.com>
2022-06-08 18:27:37 +0200

    script putfnct

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,11 +4,17 @@
  *
  * Return: Always 0 (success)
  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int main(void)
 {
 	char word[8] = "_putchar";
 	int i;
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 	return (0);
