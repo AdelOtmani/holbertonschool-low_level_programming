1d8d2efeec9d13994f062083809a3f826afebbf4
AdelOtmani <5023@holbertonschool.com>
2022-06-08 20:41:23 +0200

    script alphabet putchar

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 
 /**
  * main - check the code
- *
+ * Description: It prints the alphabet with a new line
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-	void n;
+	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
 		_putchar(n);
