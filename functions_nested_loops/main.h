d29a3b753255b6aec5dbe3e345677617373fc4b2
AdelOtmani <5023@holbertonschool.com>
2022-06-09 15:16:49 +0200

    Update to mai

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,6 @@
-/**
- *:wq _putchar - writes the character c to stdout
- *
- *
- *Return: On success 1.
- *On error, -1 is returned, and errno is set appropriately.
- */
+#ifndef main_h
+#define main_h
 int _putchar(char c);
-
 void print_alphabet(void);
+void print_alphabet_x10(void);
+#endif
