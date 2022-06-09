9c17b587a416256a353ff9b513146acab76c290a
AdelOtmani <5023@holbertonschool.com>
2022-06-09 15:47:05 +0200

    script c lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	int l = 0;
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n == c)
+			l = 1
+	}
+	return (l);
+}
