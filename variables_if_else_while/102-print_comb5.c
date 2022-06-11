b2af9761d2dbae157fbcfa193da855ce1aeceee1
AdelOtmani <5023@holbertonschool.com>
2022-06-11 22:04:12 +0200

    script print 5

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = a; c <= 9; c++)
+			{
+				for (d = b + 1; d <= 9; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (a == 9 && b == 8)
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
