40e4cb317165bbe35db7c1dccddf87eb01a9a07c
AdelOtmani <5023@holbertonschool.com>
2022-06-11 23:17:25 +0200

    naturral

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main: check the code
+ *
+ * Return: Always 0 (success)
+ */
+
+int main()
+{
+	int n;
+	int sum = 0;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
