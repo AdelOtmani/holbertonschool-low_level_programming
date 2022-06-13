4b3255d057f9e8ee43bded89011d6d4bdb5c1f15
AdelOtmani <5023@holbertonschool.com>
2022-06-13 14:52:24 +0200

    main and exo 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		else
+			_putchar(n);
+	}
+	_putchar('\n');
+}
