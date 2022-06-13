ed34a5febc5f9c97fbb9568ecd3f0f35bef7d402
AdelOtmani <5023@holbertonschool.com>
2022-06-13 18:28:21 +0200

    exo 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	int x = 1;
+
+	while (x <= 100)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			if (x == 100)
+			{
+				printf("Buzz");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", x);
+	x++;
+	}
+	return (0);
+}
+
