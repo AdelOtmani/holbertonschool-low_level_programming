32de7ef7b2b959676d62d79c6ab1295157c9132b
AdelOtmani <5023@holbertonschool.com>
2022-06-13 14:42:44 +0200

    main and exo 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - check the code.
+ * @a: First operand
+ * @b: second arg
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
