8e23376be806f8189aeb316c2019be6492da294f
AdelOtmani <5023@holbertonschool.com>
2022-06-13 14:25:34 +0200

    add main and exo 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ * @c: First operand
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int l = 0;
+	char n;
+
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		if (n == c)
+			l = 1;
+	}
+		printf("%c: %d\n", n, l);
+		return (l);
+}
