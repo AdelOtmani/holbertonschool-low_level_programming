789cd8a2bc86fbb650eef62658a0f219441ffc1a
AdelOtmani <5023@holbertonschool.com>
2022-06-13 16:17:46 +0200

    main and exo 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - check the code.
+ * @n: first arg
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			i++;
+			j = 0;
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
