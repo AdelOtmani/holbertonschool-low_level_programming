a4d43d41260d2b3d37ea88ef2d1ee929c56cf9c3
AdelOtmani <5023@holbertonschool.com>
2022-06-15 17:40:39 +0200

    main and exo 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - check the code
+ * @a: arg is int pointer
+ * @n: arg is a int
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
+	}
+	printf("\n");
+}
