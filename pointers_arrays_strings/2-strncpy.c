be1c7d75448019ab40c0119008b2bbe3f0fc7e46
AdelOtmani <5023@holbertonschool.com>
2022-06-17 00:00:11 +0200

    main and more exo 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strncpy - check the code
+ * @dest: arg is int pointer
+ * @src: arg is a pointer
+ * @n: arg is an int
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != 0 && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
