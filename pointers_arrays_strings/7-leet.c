3c8bf3acbffb0b1bf21f793db923f436d3c069f9
AdelOtmani <5023@holbertonschool.com>
2022-06-17 17:05:24 +0200

    main and more exo 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * leet - check the code
+ * @s: arg is a char pointer
+ * Return: always 0.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "AEOTL43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == a[j] || s[i] - 32 == a[j])
+					s[i] = a[j + 5];
+		}
+	}
+	return (s);
+}
