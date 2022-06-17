984c41d1b51b542350d6fcc393479487e3c3470b
AdelOtmani <5023@holbertonschool.com>
2022-06-17 14:05:24 +0200

    main  in and more exo 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *string_toupper - check the code
+ * @a: arg is a pointer
+ * @n: arg is an int
+ * Return: always 0.
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for(i=0;s[i];i++)  
+	{
+		if(s[i]>=97 && s[i]<=122)
+		s[i]-=32;
+	}
+	return (s);
+}
