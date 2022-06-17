42590d31d61555cc8b51146e0d526dc6532c242d
AdelOtmani <5023@holbertonschool.com>
2022-06-17 05:05:19 +0200

    main and more exo 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcmp - check the code
+ * @a: arg is a pointer
+ * @n: arg is an int
+ * Return: always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+	int v;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		v = a[i];
+		a[i] = a[j];
+		a[j] = v;
+	}
+}
