dd84cca94c4521a841eb8b158856508bd22da5f1
AdelOtmani <5023@holbertonschool.com>
2022-06-21 20:39:01 +0200

    main and exo1 moremore

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memcpy - check the code
+ * @dest: arg is char pointer
+ * @src: arg is a char pointer
+ * @n: arg is an unsigned int
+ * Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
