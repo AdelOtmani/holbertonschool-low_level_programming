17ddb9373d7ead490b534b2e8fcfc017957a5af6
AdelOtmani <5023@holbertonschool.com>
2022-06-21 21:11:52 +0200

    main and exo2 moremore

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - check the code
+ * @s: arg is char pointer
+ * @c: arg is a char pointer
+ * 
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != c)
+	{
+		if (s[i] == '\0')
+			return(NULL);
+		i++;
+	}
+	return(&s[i]);
+}
