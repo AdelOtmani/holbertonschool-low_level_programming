691d04739a190af6f400a1690f1ff2598ed32ccc
AdelOtmani <5023@holbertonschool.com>
2022-06-23 11:40:33 +0200

    main and exo5 moremore

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *_strstr - locates a substring
+ *
+ * @haystack: pointer char
+ * @needle: pointer char
+ *
+ * Return: a pointer to the beginning of the located substring, or NULL if
+ * the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		if (haystack[i] == needle[i])
+		{
+			while (haystack[i] == needle[i])
+			{
+				if (needle[i+1] == '\0')
+				{
+					return (haystack);
+				}
+				i++;
+			}
+		}
+		haystack++;
+	}
+	return (NULL);
+}
