7e37f3720e4f226bc7b636e1586378982b0ff698
AdelOtmani <5023@holbertonschool.com>
2022-06-23 12:55:57 +0200

    main and exo7 moremore

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * print_diagsums - prints sum of diagonal
+ * @a: pointer int
+ * @size: pointer int
+ * Return: printf
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int si = 0;
+	int sj= 0;
+
+	for (i = 0; i <= (size *size); i = i + size + 1)
+	{
+		si = si + a[i];
+	}
+	for (j = size - 1; j < (size * size -1); j = j + size -1)
+	{
+		sj = sj + a[j];
+	}
+	printf ("%d, %d \n", si, sj);
+}
