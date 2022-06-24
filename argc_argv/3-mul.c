4831fe149e6b25216354ba456be0de82adfea724
AdelOtmani <5023@holbertonschool.com>
2022-06-24 12:51:32 +0200

    exo 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,15 +11,14 @@ int main(int argc, char *argv[])
 {
 	int i, j, res;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	i = atoi(argv[1]);
 	j = atoi(argv[2]);
 	res = i * j;
-
-	if (argc != 3)
-	{
-		printf("Error");
-		exit(0);
-	}
 	printf("%d\n", res);
 	return (0);
 }
