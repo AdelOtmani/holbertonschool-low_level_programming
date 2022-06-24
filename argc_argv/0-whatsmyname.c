e78d3d7ee64c0179761881df7f58efc205410d2e
AdelOtmani <5023@holbertonschool.com>
2022-06-24 11:03:41 +0200

    Added README.md and exo 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: arg int
+ * @argv: arg is a pointer char
+ * Return: Always 0 (sucess)
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+		printf("%s\n", argv[0]);
+			return (0);
+}
