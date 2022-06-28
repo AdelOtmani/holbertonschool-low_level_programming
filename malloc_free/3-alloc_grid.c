7028a1d3f4412766cd59b2a882e953bb26dcc61d
AdelOtmani <5023@holbertonschool.com>
2022-06-28 22:52:02 +0200

    fix for loop

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,11 +13,7 @@ int **alloc_grid(int width, int height)
 	int **s;
 	int i, j;
 
-	if (width <= 0)
-	{
-		return (NULL);
-	}
-	if (height <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
