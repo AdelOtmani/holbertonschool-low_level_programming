6414af3b00b543dd90960798581d28bac3392865
AdelOtmani <5023@holbertonschool.com>
2022-06-29 02:14:55 +0200

    main and exo 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - Check the code
+ * @b: is an unsigned int
+ * Return: not with void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *m = malloc(b);
+
+	if (m = NULL)
+	{
+		exit(98);
+	}
+	return (m);
+}
