1e45ea4499b5fb039b081dcfe2e5e2816484548d
AdelOtmani <5023@holbertonschool.com>
2022-06-30 01:50:40 +0200

    main and exo2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,21 +9,23 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *p;
+	void *p1;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	p = malloc(nmemb * size);
-	if (p == NULL)
+	p1 = malloc(nmemb * size);
+	if (p1 == NULL)
 	{
 		return (NULL);
 	}
+	p = p1;
 	for (i = 0; i < (nmemb * size); i++)
 	{
-		p[i] = 0;
+		p[i] = '\0';
 	}
 	return (p);
 }
