195f682588ce90a4e4cbf003ad7877800f292483
AdelOtmani <5023@holbertonschool.com>
2022-06-29 20:51:36 +0200

    main and exo 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[size++] = s1[i];
 	}
-	for (i = 0; s2[i] < n && i < n; i++)
+	for (i = 0; s2[i] != '\0' && i < n; i++)
 	{
 		s[size++] = s2[i];
 	}
