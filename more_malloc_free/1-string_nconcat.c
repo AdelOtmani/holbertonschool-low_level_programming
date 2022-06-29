b94e088e7189795648e979f9ae64de0050bb9ad0
AdelOtmani <5023@holbertonschool.com>
2022-06-29 20:48:54 +0200

    main and exo 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *malloc_checked - Check the code
+ * @b: is an unsigned int
+ * Return: not with void
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size = n, i;
+	char *s;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (i = 0; s1[i]; i++)
+		size++;
+
+	s = malloc(sizeof(char) * (size + 1));
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	size = 0;
+
+	for (i = 0; s1[i]; i++)
+	{
+		s[size++] = s1[i];
+	}
+	for (i = 0; s2[i] < n && i < n; i++)
+	{
+		s[size++] = s2[index];
+	}
+	s[i] = '\0';
+
+	return (s);
+}
