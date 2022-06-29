49f46b0cdc525cfc1e8928aa7e57c4a3e3f7df85
AdelOtmani <5023@holbertonschool.com>
2022-06-30 01:29:59 +0200

    main and exo 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * *_calloc - Check the code
+ * @nmemb: unsigned int arg
+ * @size: unsigned int arg
+ * Return: nothing with void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *p;
+	unsigned int i;
+
+	if (nmemb || size)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
+
