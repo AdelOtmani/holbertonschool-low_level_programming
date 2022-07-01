2c101c1fba9fdf2c7d36f73af306add4305b1927
AdelOtmani <5023@holbertonschool.com>
2022-07-01 14:48:15 +0200

    README.md and exo 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - struct
+ * @name: name
+ * @float: float
+ * @owner: owner
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
 #endif
