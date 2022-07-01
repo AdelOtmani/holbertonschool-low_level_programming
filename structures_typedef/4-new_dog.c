2230bde9414646b00c171b4f4d269eda0b37b695
AdelOtmani <5023@holbertonschool.com>
2022-07-01 16:29:53 +0200

    dog.h exo 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stddef.h>
+#include <string.h>
+/**
+ * *new_dog - Check the code
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int s;
+	int n;
+	dog_t *dolgie;
+	
+	dolgie = malloc(sizeof(dog_t));
+	s = strlen(name) + 1;
+	n = strlen(owner) + 1; 
+	dolgie->name = malloc(sizeof(char) * s);
+
+	if (dolgie->name == NULL)
+	{
+		free(dolgie);
+		return (NULL);
+	}
+	strcpy(dolgie->name, name);
+	dolgie->age = age;
+	dolgie->owner = malloc(sizeof(char) * o);
+	if (dolgie->owner == NULL)
+	{
+		free(dolgie->name);
+		free(dolgie);
+		return (NULL);
+	}
+	strcpy(dolgie->owner, owner);
+	return (dolgie);
+}
