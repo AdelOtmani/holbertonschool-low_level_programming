8c920bf4c6fda7b53f58da0b47f28a8728d8980f
AdelOtmani <5023@holbertonschool.com>
2022-07-01 19:13:14 +0200

    dog.h exo 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Check the code
+ * @d: is a struct
+ * Return: nothing because void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free((*d).owner);
+		free((*d).name);
+		free((*d));
+	}
+}
