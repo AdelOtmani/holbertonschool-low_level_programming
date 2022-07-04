adac1d66911e1f5e89fed9eae5005f0ecf477f4d
AdelOtmani <5023@holbertonschool.com>
2022-07-04 03:44:37 +0200

    exo 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - Check the code
+ * @array: int arg pointer
+ * @action: void pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
