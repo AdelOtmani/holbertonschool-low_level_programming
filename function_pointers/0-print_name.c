691d0e6a163c69ca598b4447e1b15c1159b90cfe
AdelOtmani <5023@holbertonschool.com>
2022-07-04 03:24:26 +0200

    exo 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stddef.h>
 /**
  * print_name - Check the code
  * @name: char arg pointer
@@ -9,7 +8,7 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (!f || !name)
 	{
-		return (NULL);
+		return;
 	}
 	f(name);
 }
