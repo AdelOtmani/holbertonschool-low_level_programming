fcd7dbcfb7be0a5ae3f6c2b18c7d69d1808cd5ee
AdelOtmani <5023@holbertonschool.com>
2022-07-05 23:34:07 +0200

    exo 3

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - check the code
+ * @n: int arg
+ * Return: r
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int r = 0;
+	unsigned int i;
+	va_list args;
+
+	va_start (args, n);
+	for (i = 0; i < n; i++)
+	{
+		r += (int) va_arg (args, int);
+	}
+	va_end (args);
+	return (r);
+}
