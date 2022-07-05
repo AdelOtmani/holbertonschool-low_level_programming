7164d18a0092687683a4973dffdd8752ff383c49
AdelOtmani <5023@holbertonschool.com>
2022-07-05 23:49:23 +0200

    exo 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,11 +15,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int);
-
+		printf("%d", va_arg(args, int));
 		if (separator != NULL && i != (n - 1))
+		{
 			printf ("%s", separator);
-
+		}
 	}
 	printf("\n");
 	va_end(args);
