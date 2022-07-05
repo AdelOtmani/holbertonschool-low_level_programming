274b27ea59e721e97c984c617c2358f9c29e7ba7
AdelOtmani <5023@holbertonschool.com>
2022-07-05 10:02:17 +0200

    exo 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Check the code
+ * @argc: number of arg
+ * @argv: arg char pointer
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int r;
+	int (*s)(int,int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+	}
+	if (argv[2] == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+	}
+	s = get_op_func(argv[2]);
+
+	if (s == NULL)
+	{
+		printf("Error\n");
+	}
+	r = (*s)(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", r);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - Check the code
+ * @a: int arg
+ * @b: int arg
+ * Return: the result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub: Check the code
+ * @a: int arg
+ * @b: int arg
+ * Return: the result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Check the code
+ * @a: int arg
+ * @b: int arg
+ * Return: The result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Check the code
+ * @a: int arg
+ * @b: int arg
+ * Return: the result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Check the code
+ * @a: int arg
+ * @b: int arg
+ * Return: th result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
