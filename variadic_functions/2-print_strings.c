ca29fdbcd7a5cc35ff98403330368ee7de7e60d2
AdelOtmani <5023@holbertonschool.com>
2022-07-06 00:03:06 +0200

    exo 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		c = va_arg(args, char *)
+		c = va_arg(args, char *);
 		if (c == NULL)
 		{
 			printf("(nil)");
