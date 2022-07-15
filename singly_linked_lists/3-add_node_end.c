6756b4ce01c084469240f59f38c2d98a5dee1bdd
AdelOtmani <5023@holbertonschool.com>
2022-07-15 11:38:04 +0200

    exo 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_node_end - Check the code
+ * @head: arg
+ * @str: arg
+ * Return: node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *p, *tmp;
+
+	p = malloc(sizeof(list_t));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	p->str = strdup(str);
+	p->len = strlen(str);
+	p->next = NULL;
+
+	tmp = *head;
+	if (*head == NULL)
+	{
+		*head = p;
+	}
+	else
+	{
+		while (tmp->next)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = p;
+	}
+	return (p);
+}
