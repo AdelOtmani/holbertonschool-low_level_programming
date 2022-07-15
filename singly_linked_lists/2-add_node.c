75a9046c2d3f5d2c1a4f9049c8bba4d4aac410db
AdelOtmani <5023@holbertonschool.com>
2022-07-15 02:21:20 +0200

    exo 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_note - check the code
+ * @head: arg
+ * @str: arg pointer
+ * Return: adresse of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *s;
+	int i;
+	list_t *p;
+
+	p = malloc(sizeof(list_t));
+	if (p == NULL)
+		return (NULL);
+	
+	s = strdup(str);
+	if (s == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	
+	while (str[i] != NULL)
+		i++;
+
+	p->str = s;
+	p->i = i;
+	p->next = *head;
+
+	*head = p;
+
+	return (p);
+}
