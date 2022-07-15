666643ec3491776a4f33ba48866d52c3bb86684d
AdelOtmani <5023@holbertonschool.com>
2022-07-15 15:00:31 +0200

    exo 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - check
+ * @h: arg cnst
+ * Return: i
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
