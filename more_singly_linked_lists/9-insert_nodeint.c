cf605128507712fba7f50e6e9835892532beddc5
AdelOtmani <5023@holbertonschool.com>
2022-07-16 16:10:02 +0200

    exo 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *node, *tmp, *h;
 	unsigned int i;
 
-	*tmp = *head;
+	tmp = *head;
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		tmp = tmp->next;
 	}
-	half = tmp->next;
+	h = tmp->next;
 	tmp->next = node;
 	node->next = h;
 	return (node);
