626689756e8f4079de711489e80a7431205424de
AdelOtmani <5023@holbertonschool.com>
2022-07-16 12:07:44 +0200

    exo 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: arg double pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *tmp2;
+
+	if (head == NULL)
+		return;
+	tmp2 = *head
+	while (*head)
+	{
+		tmp = tmp2->next;
+		free(*head);
+		tmp2 = tmp;
+	}
+
+	head = NULL;
+}
