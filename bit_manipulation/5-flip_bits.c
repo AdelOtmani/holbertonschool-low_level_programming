2f95431173a05ad0c1e3c1c6776b49d04c7a1d70
AdelOtmani <5023@holbertonschool.com>
2022-07-20 21:21:08 +0200

    exo 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - Check the code
+ * @n: arg int
+ * @m: arg int
+ * Return: i
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m; 
+	unsigned long int i;
+
+	for (i = 0; xor > 0;)
+	{
+		i += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (i);
+}
