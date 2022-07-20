d9da341663dc675b095eb7adcd93fea8e5d5f53d
AdelOtmani <5023@holbertonschool.com>
2022-07-20 13:38:53 +0200

    exo 0 and condition change

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val = 0;
-	int i = 0;
+	int i = 0, n;
 
 	if (b == NULL)
 		return (0);
+	n = strlen(b) - 1;
 
-	while (b[i] == '0' || b[i] == '1')
+	while (n >= 0)
 	{
-		val <<= 1;
-		val += b[i] - '0';
-		i++;
+		if (b[i] == '0' || b[i] == '1')
+		{
+			val <<= 1;
+			val += b[i] - '0';
+			i++;
+		}
+		else
+			return (0);
+		n--;
 	}
 	return (val);
 }
