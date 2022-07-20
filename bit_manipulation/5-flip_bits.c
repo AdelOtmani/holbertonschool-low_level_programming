f8f5222263df566c644086fa303d3c8d117dc497
AdelOtmani <5023@holbertonschool.com>
2022-07-20 21:22:15 +0200

    exo 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,7 +7,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int xor = n ^ m; 
+	unsigned long int xor = n ^ m;
 	unsigned long int i;
 
 	for (i = 0; xor > 0;)
