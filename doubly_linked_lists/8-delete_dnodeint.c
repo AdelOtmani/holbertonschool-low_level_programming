7d20890ab3be02fe112626b8e1f22720b023f066
AdelOtmani <5023@holbertonschool.com>
2022-07-22 21:40:08 +0200

    white space deleted

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,7 +23,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			tmp->next->prev = NULL;
 		}
 		free(tmp);
-		return (1);	
+		return (1);
 	}
 
 	while (tmp && i < index - 1)
