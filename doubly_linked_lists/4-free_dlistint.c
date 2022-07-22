8deebe803beb7243a88eeff3074ccad1f35dd375
AdelOtmani <5023@holbertonschool.com>
2022-07-22 04:25:35 +0200

    exo 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - Frees list
+ * @head: arg pointer
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
