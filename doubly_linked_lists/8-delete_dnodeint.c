4f229df3047d62da7d296fbf218160891bbb39a0
AdelOtmani <5023@holbertonschool.com>
2022-07-22 22:18:42 +0200

    trying to win

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (1);
 	}
 	for (; tmp->next != NULL && i < index; tmp = tmp->next, i++)
-	if (tmp->next == NULL || i < index)
+	if (tmp->next == NULL && i < index)
 		return (-1);
 	tmp->next->prev = tmp->prev;
 	tmp->prev->next = tmp->next;
