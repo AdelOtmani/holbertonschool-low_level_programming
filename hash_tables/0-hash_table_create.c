5a69839b5a460af8c40a27368b593200315ae446
AdelOtmani <5023@holbertonschool.com>
2022-08-21 22:09:51 +0200

    exo 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,7 +7,6 @@
  * Return: Returns a pointer to the newly created hash table
  * If something went wrong, your function should return NULL
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_htable;
