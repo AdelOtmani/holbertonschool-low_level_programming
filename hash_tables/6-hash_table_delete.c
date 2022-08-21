81e67b157c824eb07516e16c2ebc3c1712d6031a
AdelOtmani <5023@holbertonschool.com>
2022-08-22 00:00:09 +0200

    exo 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -20,7 +20,7 @@ void hash_table_delete(hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				tmp = node->next;
+				temp = node->next;
 				free(node->key);
 				free(node->value);
 				free(node);
