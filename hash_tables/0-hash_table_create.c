53667d66e5fac690236011b417b9ed09f6c80ebe
AdelOtmani <5023@holbertonschool.com>
2022-08-21 21:53:14 +0200

    exo 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash.h"
+/**
+ * hash_table_create - Function main
+ * @size: long int arg
+ * Return: Returns a pointer to the newly created hash table
+ * If something went wrong, your function should return NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_htable;
+
+	if (size != 0)
+	{
+		return (NULL);
+	}
+	new_htable = malloc(sizeof(hash_table_t));
+	if (new_htable == NULL)
+	{
+		return (NULL);
+	}
+	new_htable->size;
+	new_htable->array == malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (unsigned long int i = 0; i < size; i++)
+	{
+		new_htable->array[i] = NULL;
+	}
+	return (new_htable);
+}
