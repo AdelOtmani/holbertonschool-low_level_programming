8cf3d3cfa30409180d1720dea9c8688f963b9243
AdelOtmani <5023@holbertonschool.com>
2022-08-26 15:15:28 +0200

    exo 1

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+* binary_search - Function
+* @array: array to search
+* @size: size of the array
+* @value: value to search for
+* Return: the first index where value is located
+*/
+
+/**
+* binary_search - Function
+* @array: array to search
+* @size: size of the array
+* @value: value to search for
+* Return: the first index where value is located
+*/
+int binary_search_recu(int *array, size_t b, size_t e, int value)
+{
+	unsigned long int  m, i;
+	char *d;
+
+	d = " ";
+	m = (b + (e - 1)) / 2;
+	if (b >= e)
+	{
+		return (-1);
+	}
+	printf("Searching in array:");
+	for (i = b; i < e; i++)
+	{
+		printf("%s%d", d, array[i]);
+		d =", ";
+	}
+	printf ("\n");
+	if (array[m] == value)
+	{
+		return (m);
+	}
+	if (array[m] < value)
+	{
+		return (binary_search_recu(array, m + 1, e, value));
+	}	
+	return (binary_search_recu(array, b, m, value));
+
+}
+
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (binary_search_recu(array, 0, size, value));
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
+#include <stdlib.h>
+#include <stdio.h>
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int binary_search_recu(int *array, size_t b, size_t e, int value);
 #endif
